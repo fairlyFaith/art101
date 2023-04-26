dc6123fcd5ac792e52d547bdce7e79d89e6c8611
Faith <faithsmith265@gmail.com>
2023-04-25 23:38:09 -0700

    lab5jsfix

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -29,9 +29,9 @@ age = 2023 - year;
 
 console.log ("Age " + age)
 
-document.writeln("Make " + make + "<br>");
-document.writeln("Model " + model + "<br>");
-document.writeln("Color " + color + "<br>");
-document.writeln("Year " + year + "<br>");
-document.writeln("Age " + age + " years<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
