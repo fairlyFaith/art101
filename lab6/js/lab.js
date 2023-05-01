e3e6ceca7ee3246abadd2908e8b82bda4894a65d
Faith <faithsmith265@gmail.com>
2023-05-01 15:19:20 -0700

    lab6changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// index.js - Using Arrays and Objects
+// Author: Faith Smith
+// Date: 5/1/2023
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  make = "Santa Cruz Metro";
+  model = "Public Bus";
+  color = "white";
+  year = 2018;
+  age = 2023 - year;
+
+  myTransport = ["bus", " friends car", " uber", " bike"];
+
+  var myMainRide = {
+    make: "Santa Cruz Metro",
+    model: "Public Bus",
+    color: "white",
+    year: 2018,
+    age: function() {
+        return 2022 - this.year;
+    }
+  }
+
+  myMainRide.age() 
+
+  document.writeln("Kinds of Transportation I use: " + myTransport + "<br>");
+  document.writeln("My main ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
+
 
 // let's get this party started
 main();
