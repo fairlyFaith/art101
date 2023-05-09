82a6eaed37dc4d821c673656d314f09768c140f1
Faith <faithsmith265@gmail.com>
2023-05-08 22:42:21 -0700

    lab8edits

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,4 +1,4 @@
-// index.js - 
+// index.js - Anon and Callback Functions
 // Author: Faith Smith
 // Date: 5/3/2023
 
@@ -6,16 +6,49 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+
+  //Function that multiples number by 10
+    function Times10(x){
+    var Result = (x*10);
+    return Result;
+  }
+
+  //Test for function
+  console.log("Testing if function works: 4 x 10 = " + Times10(4));
+
+  console.log("Testing if function works: 564 x 10 = " + Times10(564));
+
+  //array of numbers being put in list
+  var arraylist = array = [2, 5, 90, 100, 58932, 984628193];
+  
+  //console log showing array
+  console.log("My Array: " + array);
+  
+  var multi = array.map(Times10);
+
+  //console log showinf arrayx10
+  console.log("My Array x 10: " + array.map(Times10));
+
+  var anon = array.map(function(x){
+    var results = (x**8);
+    return results;
+  })
+
+  console.log("My Array ^8: " + anon);
+
+
+  var outputEl = document.getElementById("output"); 
+  outputEl.innerHTML = ("My array: " + arraylist +
+  " <br> " + "My numbers times 10: " + multi +
+  " <br> " + "My numbers to the 8th power: " + anon); // put your results here
+
 }
 
 // let's get this party started
 main();
+
+
+
