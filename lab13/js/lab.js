9c002f93339b0d07df4c0605a3cb3732d44947b6
Faith <faithsmith265@gmail.com>
2023-05-23 22:29:34 -0700

    lab13edits

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -2,19 +2,45 @@
 // Author: Faith Smith
 // Date: 5/3/2023
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
+  var myButton = document.getElementById("button");
+  myButton.addEventListener("click", function fizzBuzz() {
+  
+    for (var i=1; i<=200; i++) {
+      
+      var str = '';
+    
+      if (i % 3 == 0) {
+        str += "Fizz";
+      }
+      
+      if (i % 5 == 0) {
+        str += "Buzz";
+      }
+     
+      if (i % 7 == 0) {
+        str += "Boom";
+      }
+
+      if (str == '') {
+        str += i;
+      } 
+      
+      else {
+        str += "!";
+      }
+
+      debugger;
+      $("#output").append("<p>" + str + "</p>")
+    }
+  })
+  
+  fizzBuzz();
 }
 
 // let's get this party started
