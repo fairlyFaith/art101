979097dcf669d74689b06303922778681fdad950
Faith <faithsmith265@gmail.com>
2023-05-31 19:14:55 -0700

    lab15edits

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,20 +1,29 @@
-// index.js - 
+// index.js - 63
 // Author: Faith Smith
 // Date: 5/3/2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+    
+  function getStuff() {
+    console.log("Button pushed");
+    $.ajax({
+      url: "https://yesno.wtf//api",
+      type: "GET",
+      data: {},
+    })
+    .done(function(data) {
+       // console.log(data);
+      console.log(data);
+      var answer = data.answer;
+      var imgURL = data.image;
+      $("#output").html("<h2>" + answer);
+      $("#output").append("<img src=" + imgURL + ">");
+    })
+  }
+  
+  $("button").click(getStuff);
+
 }
 
 // let's get this party started
