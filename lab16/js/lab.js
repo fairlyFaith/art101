075907f35ea557d34fa8161e739527ea79713db8
Faith <faithsmith265@gmail.com>
2023-06-06 01:30:50 -0700

    lab16edits

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,4 +1,4 @@
-// index.js - 
+// index.js - JSON and API's
 // Author: Faith Smith
 // Date: 5/3/2023
 
@@ -6,16 +6,68 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+var URL = "https://xkcd.com/info.0.json";
+
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
+
+function make_safe(str) {
+  return str.replace(/'/g, '&apos;').replace(/"/g, '&quot;');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+function getComic(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+
+  var ourURL = URLpre + numStr + URLpost;
+  console.log("Our new URL:", ourURL);
+ 
+  $.ajax({
+      url: ourURL,
+      type: "GET",
+  })
+
+
+  .done(function(data) {
+
+      var imageUrl = data.img;
+    
+      var title = data.title;
+      console.log("orig title:", title);
+      title = make_safe(title);
+      console.log("safe title:", title);
+      var alt = data.alt;
+      console.log("orig alt:", alt);
+      alt = make_safe(alt);
+      console.log("safe alt:", alt);
+      var comicNum = data.num;
+
+      var html = `<div id="imageblock">
+          <h2>${title}</h2>
+          <img src="${imageUrl}" title="${alt}"><br>
+          <button id="prev">Previous</button><button id="next">Next</button>
+        </div>`
+
+      $("#output").html(html);
+
+      $("#prev").click(function(){
+        getComic(comicNum - 1);
+      });
+
+      $("#next").click(function(){
+        getComic(comicNum + 1);
+      });
+  })
+
+
+  .fail(function(){
+    console.log("^^ Please ignore this error. It's okay.");
+    console.log("Have a okay day! :-)");
+  })
+
 }
 
-// let's get this party started
-main();
+getComic();
