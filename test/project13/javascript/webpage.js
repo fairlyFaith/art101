5470c8be0788f4a13d566a349dbb22f213fa8653
Faith <faithsmith265@gmail.com>
2023-06-12 16:36:46 -0700

    testchanges

--- test/project13/javascript/webpage.js
+++ test/project13/javascript/webpage.js
@@ -172,6 +172,15 @@ Array.from(sidebarLinks).forEach(link => {
 
 
 
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
 let map, infoWindow;
 
 function initMap() {
